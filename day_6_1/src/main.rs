974dcd11575ac3127788b52555df57add6eb3f64
camerongraybill <camerongraybill@gmail.com>
2020-12-06 11:36:15 -0500

    Part 1 done

--- day_6_1/src/main.rs
+++ day_6_1/src/main.rs
@@ -1,25 +1,20 @@
 use clap::{Arg, App};
 use std::str::FromStr;
-use std::num::ParseIntError;
 use std::fs;
-use std::collections::{HashMap};
+use std::collections::{HashSet};
 
-struct Passport {
-    data: HashMap<String, String>,
+struct DeclarationGroup {
+    data: HashSet<u8>,
 }
 
-impl FromStr for Passport {
-    type Err = ParseIntError;
+impl FromStr for DeclarationGroup {
+    type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Passport {
+        Ok(DeclarationGroup {
             data: s
-                .split_whitespace()
-                .map(
-                    |pair|pair.split(':')
-                        .map(|s|s.to_string())
-                )
-                .map(|mut pair| (pair.next().unwrap(), pair.next().unwrap()))
+                .bytes()
+                .filter(|b| *b != b'\n')
                 .collect()
         })
     }
@@ -30,12 +25,44 @@ impl FromStr for Passport {
 mod tests {
     use super::*;
     #[test]
-    fn test_passport_fromstr() -> Result<(), String> {
-        let passport = "a:b c:d\ne:f".parse::<Passport>().unwrap();
-        assert_eq!(passport.data.keys().count(), 3);
-        assert!(passport.data.contains_key("a"));
-        assert!(passport.data.contains_key("c"));
-        assert!(passport.data.contains_key("e"));
+    fn test_declaration_group_fromstr_1() -> Result<(), String> {
+        let passport = "abc".parse::<DeclarationGroup>().unwrap();
+        assert_eq!(passport.data.len(), 3);
+        assert!(passport.data.contains(&b'a'));
+        assert!(passport.data.contains(&b'b'));
+        assert!(passport.data.contains(&b'c'));
+        Ok(())
+    }
+    #[test]
+    fn test_declaration_group_fromstr_2() -> Result<(), String> {
+        let passport = "a\nb\nc".parse::<DeclarationGroup>().unwrap();
+        assert_eq!(passport.data.len(), 3);
+        assert!(passport.data.contains(&b'a'));
+        assert!(passport.data.contains(&b'b'));
+        assert!(passport.data.contains(&b'c'));
+        Ok(())
+    }
+    #[test]
+    fn test_declaration_group_fromstr_3() -> Result<(), String> {
+        let passport = "ab\nac".parse::<DeclarationGroup>().unwrap();
+        assert_eq!(passport.data.len(), 3);
+        assert!(passport.data.contains(&b'a'));
+        assert!(passport.data.contains(&b'b'));
+        assert!(passport.data.contains(&b'c'));
+        Ok(())
+    }
+    #[test]
+    fn test_declaration_group_fromstr_4() -> Result<(), String> {
+        let passport = "a\na\na\na".parse::<DeclarationGroup>().unwrap();
+        assert_eq!(passport.data.len(), 1);
+        assert!(passport.data.contains(&b'a'));
+        Ok(())
+    }
+    #[test]
+    fn test_declaration_group_fromstr_5() -> Result<(), String> {
+        let passport = "b".parse::<DeclarationGroup>().unwrap();
+        assert_eq!(passport.data.len(), 1);
+        assert!(passport.data.contains(&b'b'));
         Ok(())
     }
 
@@ -46,10 +73,14 @@ fn main() {
     let args = App::new("Day six part one of AOC 2020!!")
         .arg(Arg::with_name("input-file").takes_value(true))
         .get_matches();
-    let input_data = fs::read_to_string(
+    let total_count: usize = fs::read_to_string(
         args.value_of("input-file").unwrap()
     )
-        .unwrap();
-    println!("{}", input_data);
+        .unwrap()
+        .split("\n\n")
+        .map(|group| group.parse::<DeclarationGroup>().unwrap())
+        .map(|group| group.data.len())
+        .sum();
+    println!("{}", total_count);
 }
 
