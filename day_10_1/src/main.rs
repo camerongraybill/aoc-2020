3e2fafeed974fb63728f75e0b96a1aae42036c52
camerongraybill <camerongraybill@gmail.com>
2020-12-10 10:48:07 -0500

    Removed unused code

--- day_10_1/src/main.rs
+++ day_10_1/src/main.rs
@@ -4,36 +4,6 @@ use std::str::FromStr;
 use counter::Counter;
 
 
-type Joltage = u32;
-
-struct Adapter {
-    output_joltage: Joltage,
-}
-
-impl Adapter {
-    fn can_pull_from(&self, source_joltage: Joltage) -> bool {
-        if self.output_joltage == 0 {
-            source_joltage == 0
-        } else if self.output_joltage < 3 {
-            source_joltage <= (self.output_joltage - 1)
-        } else {
-            source_joltage >= (self.output_joltage - 3) && source_joltage <= (self.output_joltage - 1)
-        }
-    }
-}
-
-impl FromStr for Adapter {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(
-            Adapter {
-                output_joltage: s.parse().unwrap()
-            }
-        )
-    }
-}
-
 fn main() {
     let args = App::new("Day ten part one of AOC 2020!!")
         .arg(Arg::with_name("input-file").takes_value(true))
