2cb95a063155cf868c8eed0a0a3100b032d9b7f4
camerongraybill <camerongraybill@gmail.com>
2020-12-10 10:45:34 -0500

    Done with part 1 of day 10!

--- day_10_1/src/main.rs
+++ day_10_1/src/main.rs
@@ -1,59 +1,61 @@
 use clap::{Arg, App};
 use std::fs;
+use std::str::FromStr;
+use counter::Counter;
 
-fn chunk_is_satisfied(chunk: &[usize], target: usize) -> bool {
-    for idx_a in 0..chunk.len() {
-        for idx_b in 0..chunk.len() {
-            if chunk[idx_a] + chunk[idx_b] == target {
-                return true;
-            }
-        }
-    }
-    false
+
+type Joltage = u32;
+
+struct Adapter {
+    output_joltage: Joltage,
 }
 
-
-fn find_first_broken_idx(data: &Vec<usize>, preamble_size: usize) -> usize {
-    (preamble_size..data.len())
-        .map(|idx_in_data| (&data[idx_in_data - preamble_size..idx_in_data], idx_in_data))
-        .find(|(chunk, idx_in_data)| !chunk_is_satisfied(chunk, data[*idx_in_data]))
-        .unwrap()
-        .1
-}
-
-fn find_contiguous_addition(data: &Vec<usize>, target: usize) -> &[usize] {
-    for starting_idx in 0..data.len() {
-        for end_idx in starting_idx..data.len() {
-            let range = &data[starting_idx..=end_idx];
-            let sum: usize = range.iter().sum();
-            if sum > target {
-                break;
-            }
-            if sum == target {
-                return range;
-            }
+impl Adapter {
+    fn can_pull_from(&self, source_joltage: Joltage) -> bool {
+        if self.output_joltage == 0 {
+            source_joltage == 0
+        } else if self.output_joltage < 3 {
+            source_joltage <= (self.output_joltage - 1)
+        } else {
+            source_joltage >= (self.output_joltage - 3) && source_joltage <= (self.output_joltage - 1)
         }
     }
-    unreachable!()
+}
+
+impl FromStr for Adapter {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(
+            Adapter {
+                output_joltage: s.parse().unwrap()
+            }
+        )
+    }
 }
 
 fn main() {
     let args = App::new("Day ten part one of AOC 2020!!")
         .arg(Arg::with_name("input-file").takes_value(true))
-        .arg(Arg::with_name("preamble-size").takes_value(true))
         .get_matches();
-    let input_data: Vec<usize> = fs::read_to_string(
+    let mut input_data: Vec<usize> = fs::read_to_string(
         args.value_of("input-file").unwrap()
     )
         .unwrap()
         .lines()
         .map(|line| line.parse().unwrap())
         .collect();
-    let preamble_size: usize = args.value_of("preamble-size").unwrap().parse().unwrap();
-    let part_one_answer = input_data[find_first_broken_idx(&input_data, preamble_size)];
-    let part_two_range = find_contiguous_addition(&input_data, part_one_answer);
+    input_data.sort();
+    input_data.insert(0, 0);
+    input_data.push(input_data.last().unwrap() + 3);
+    let counted: Counter<_> = input_data[0..input_data.len() - 1]
+        .iter()
+        .zip(input_data[1..input_data.len()].iter())
+        .map(|(lower_index, higher_index)| higher_index - lower_index)
+        .collect();
 
 
-    println!("{}", part_two_range.iter().min().unwrap() + part_two_range.iter().max().unwrap());
+
+    println!("{}", counted[&1] * counted[&3]);
 }
 
