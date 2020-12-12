a5a4b210a059a34e07c1d9dbbcffa5c8151012ab
camerongraybill <camerongraybill@gmail.com>
2020-12-12 13:22:33 -0500

    Done with part 2

--- day_12_2/src/main.rs
+++ day_12_2/src/main.rs
@@ -2,6 +2,8 @@ use clap::{Arg, App};
 use std::fs;
 use std::str::FromStr;
 
+type CoordInt = i128;
+
 #[derive(Copy, Clone)]
 enum Direction {
     North = 0,
@@ -10,24 +12,12 @@ enum Direction {
     West = 3,
 }
 
-fn direction_from_degrees(degrees: i32) -> Direction {
-    match ((degrees + 360 * 2) % 360) / 90 {
-        0 => Direction::North,
-        1 => Direction::East,
-        2 => Direction::South,
-        3 => Direction::West,
-        _ => {println!("{}", degrees); unreachable!()}
-    }
-}
-
-fn turn_direction(direction: Direction, degrees: i32) -> Direction {
-    direction_from_degrees((direction as i32) * 90 + degrees)
-}
-
+#[derive(Debug)]
 struct ShipState {
-    x_pos: i32,
-    y_pos: i32,
-    facing: Direction
+    x_pos: CoordInt,
+    y_pos: CoordInt,
+    waypoint_x: CoordInt,
+    waypoint_y: CoordInt,
 }
 
 impl ShipState {
@@ -36,52 +26,64 @@ impl ShipState {
         ShipState {
             x_pos: 0,
             y_pos: 0,
-            facing: Direction::East,
+            waypoint_x: 10,
+            waypoint_y: 1,
         }
     }
 
-    fn apply_action(&self, action: Action) -> ShipState {
+    fn rotate_waypoint_by(self, degrees: CoordInt) -> ShipState {
+        (0..(((degrees + 360 * 2) % 360) / 90))
+            .fold(self, |state, _| state.rotate_waypoint_by_90())
+    }
+
+    fn rotate_waypoint_by_90(self) -> ShipState {
+        ShipState {
+            x_pos: self.x_pos,
+            y_pos: self.y_pos,
+            waypoint_x: self.waypoint_y,
+            waypoint_y: self.waypoint_x * -1,
+        }
+    }
+
+
+    fn apply_action(self, action: Action) -> ShipState {
         match action {
-            Action::MoveDirection(direction, amount) => {
-                ShipState {
-                    x_pos: match direction {
-                        Direction::North => self.x_pos + (amount as i32),
-                        Direction::South => self.x_pos - (amount as i32),
-                        _ => self.x_pos,
-                    },
-                    y_pos: match direction {
-                        Direction::East => self.y_pos + (amount as i32),
-                        Direction::West => self.y_pos - (amount as i32),
-                        _ => self.y_pos,
-                    },
-                    facing: self.facing
-                }
-            }
-            Action::Left(degrees) => {
+            Action::MoveWaypoint(direction, amount) => {
                 ShipState {
                     x_pos: self.x_pos,
                     y_pos: self.y_pos,
-                    facing: turn_direction(self.facing, -1 * (degrees as i32))
+                    waypoint_x: match direction {
+                        Direction::East => self.waypoint_x + (amount as CoordInt),
+                        Direction::West => self.waypoint_x - (amount as CoordInt),
+                        _ => self.waypoint_x,
+                    },
+                    waypoint_y: match direction {
+                        Direction::North => self.waypoint_y + (amount as CoordInt),
+                        Direction::South => self.waypoint_y - (amount as CoordInt),
+                        _ => self.waypoint_y,
+                    },
                 }
             }
-            Action::Right(degrees) => {
+            Action::Left(degrees) => self.rotate_waypoint_by(-1 * (degrees as CoordInt)),
+            Action::Right(degrees) => self.rotate_waypoint_by(degrees as CoordInt),
+            Action::Forward(amount) => {
                 ShipState {
-                    x_pos: self.x_pos,
-                    y_pos: self.y_pos,
-                    facing: turn_direction(self.facing, degrees as i32)
+                    x_pos: self.x_pos + (amount as CoordInt) * self.waypoint_x,
+                    y_pos: self.y_pos + (amount as CoordInt) * self.waypoint_y,
+                    waypoint_y: self.waypoint_y,
+                    waypoint_x: self.waypoint_x,
                 }
             }
-            Action::Forward(amount) => self.apply_action(Action::MoveDirection(self.facing, amount))
         }
     }
 
-    fn manhattan_distance(&self) -> usize {
+    fn manhattan_distance(self) -> usize {
         (self.y_pos.abs() + self.x_pos.abs()) as usize
     }
 }
 
 enum Action {
-    MoveDirection(Direction, u16),
+    MoveWaypoint(Direction, u16),
     Left(u16),
     Right(u16),
     Forward(u16),
@@ -93,10 +95,10 @@ impl FromStr for Action {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let val = s[1..].parse::<u16>().unwrap();
         match s.bytes().next().unwrap() {
-            b'N' => Ok(Action::MoveDirection(Direction::North, val)),
-            b'S' => Ok(Action::MoveDirection(Direction::South, val)),
-            b'E' => Ok(Action::MoveDirection(Direction::East, val)),
-            b'W' => Ok(Action::MoveDirection(Direction::West, val)),
+            b'N' => Ok(Action::MoveWaypoint(Direction::North, val)),
+            b'S' => Ok(Action::MoveWaypoint(Direction::South, val)),
+            b'E' => Ok(Action::MoveWaypoint(Direction::East, val)),
+            b'W' => Ok(Action::MoveWaypoint(Direction::West, val)),
             b'L' => Ok(Action::Left(val)),
             b'R' => Ok(Action::Right(val)),
             b'F' => Ok(Action::Forward(val)),
@@ -110,7 +112,45 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_parsing() -> Result<(), String> {
+    fn test_example() -> Result<(), String> {
+        let mut ship_state = ShipState::new();
+        assert_eq!(ship_state.y_pos, 0);
+        assert_eq!(ship_state.x_pos, 0);
+        assert_eq!(ship_state.waypoint_x, 10);
+        assert_eq!(ship_state.waypoint_y, 1);
+        ship_state = ship_state.apply_action(Action::Forward(10));
+        assert_eq!(ship_state.y_pos, 10);
+        assert_eq!(ship_state.x_pos, 100);
+        assert_eq!(ship_state.waypoint_x, 10);
+        assert_eq!(ship_state.waypoint_y, 1);
+        ship_state = ship_state.apply_action(Action::MoveWaypoint(Direction::North, 3));
+        assert_eq!(ship_state.y_pos, 10);
+        assert_eq!(ship_state.x_pos, 100);
+        assert_eq!(ship_state.waypoint_x, 10);
+        assert_eq!(ship_state.waypoint_y, 4);
+        ship_state = ship_state.apply_action(Action::Forward(7));
+        assert_eq!(ship_state.y_pos, 38);
+        assert_eq!(ship_state.x_pos, 170);
+        assert_eq!(ship_state.waypoint_x, 10);
+        assert_eq!(ship_state.waypoint_y, 4);
+        ship_state = ship_state.apply_action(Action::Right(90));
+        assert_eq!(ship_state.y_pos, 38);
+        assert_eq!(ship_state.x_pos, 170);
+        assert_eq!(ship_state.waypoint_x, 4);
+        assert_eq!(ship_state.waypoint_y, -10);
+        ship_state = ship_state.apply_action(Action::Forward(11));
+        assert_eq!(ship_state.y_pos, -72);
+        assert_eq!(ship_state.x_pos, 214);
+        assert_eq!(ship_state.waypoint_x, 4);
+        assert_eq!(ship_state.waypoint_y, -10);
+        ship_state = ship_state.apply_action(Action::Left(180));
+        assert_eq!(ship_state.y_pos, -72);
+        assert_eq!(ship_state.x_pos, 214);
+        assert_eq!(ship_state.waypoint_x, -4);
+        assert_eq!(ship_state.waypoint_y, 10);
+
+
+        assert_eq!(ship_state.manhattan_distance(), 286);
         Ok(())
     }
 
