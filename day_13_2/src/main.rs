307fa2ec53db2129140defaeac98af776e39c337
camerongraybill <camerongraybill@gmail.com>
2020-12-13 12:57:33 -0500

    Solved with wolfram alpha lul

--- day_13_2/src/main.rs
+++ day_13_2/src/main.rs
@@ -2,9 +2,24 @@ use clap::{Arg, App};
 use std::fs;
 use std::str::FromStr;
 
+enum Bus {
+    Unscheduled,
+    Scheduled(usize),
+}
+
 struct InputData {
-    arrive_at: usize,
-    busses: Vec<usize>,
+    busses: Vec<Bus>,
+}
+
+impl FromStr for Bus {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "x" => Ok(Bus::Unscheduled),
+            other => Ok(Bus::Scheduled(other.parse().unwrap()))
+        }
+    }
 }
 
 impl FromStr for InputData {
@@ -12,13 +27,12 @@ impl FromStr for InputData {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut lines_iter = s.lines();
+        lines_iter.next();
         Ok(
             InputData {
-                arrive_at: lines_iter.next().unwrap().parse().unwrap(),
                 busses: lines_iter.next()
                     .unwrap()
                     .split(',')
-                    .filter(|split| *split != "x")
                     .map(|num| num.parse().unwrap()).collect()
             }
         )
@@ -26,16 +40,10 @@ impl FromStr for InputData {
 }
 
 impl InputData {
-    fn first_departure(self) -> usize {
-        for num in self.arrive_at.. {
-            for bus in &self.busses {
-                if num % bus == 0 {
-                    return bus * (num - self.arrive_at);
-                }
-            }
-        }
-        unreachable!()
+    fn find_consecutive_timestamp(self) -> usize {
+        0
     }
+
 }
 
 #[cfg(test)]
@@ -43,11 +51,61 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example() -> Result<(), String> {
-        let example_data: InputData = "939
+    fn test_example_1() -> Result<(), String> {
+        let example_data: InputData = "
 7,13,x,x,59,x,31,19
 ".parse().unwrap();
-        assert_eq!(example_data.first_departure(), 295);
+        assert_eq!(example_data.find_consecutive_timestamp(), 1068781);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_2() -> Result<(), String> {
+        let example_data: InputData = "
+17,x,13,19
+".parse().unwrap();
+        assert_eq!(example_data.find_consecutive_timestamp(), 3417);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_3() -> Result<(), String> {
+        let example_data: InputData = "
+67,7,59,61
+".parse().unwrap();
+        assert_eq!(example_data.find_consecutive_timestamp(), 754018);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_4() -> Result<(), String> {
+        let example_data: InputData = "
+67,x,7,59,61
+".parse().unwrap();
+        assert_eq!(example_data.find_consecutive_timestamp(), 779210);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_5() -> Result<(), String> {
+        let example_data: InputData = "
+67,7,x,59,61
+".parse().unwrap();
+        assert_eq!(example_data.find_consecutive_timestamp(), 1261476);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_example_6() -> Result<(), String> {
+        let example_data: InputData = "
+1789,37,47,1889
+".parse().unwrap();
+        assert_eq!(example_data.find_consecutive_timestamp(), 1202161486);
 
         Ok(())
     }
@@ -64,6 +122,6 @@ fn main() {
         .unwrap()
         .parse()
         .unwrap();
-    println!("{}", input_data.first_departure())
+    println!("{}", input_data.find_consecutive_timestamp())
 }
 
